fbc5411eb815637359cc968154c5e6b55867d203
free <oncwnuM3kr4gzkR8gi52AQSjKbTU@git.weixin.qq.com>
2021-01-22 17:28:09 +0800

    recheckout

---
+++ src/main/java/com/newstarlab/vaccinebooking/VaccinebookApplication.java
@@ -0,0 +1,13 @@
+package com.newstarlab.vaccinebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinebookApplication.class, args);
+	}
+
+}
